0b706a031f85905cca946d2c1f8dda1bd652592c
V21-N <vinnr21@gmail.com>
2026-09-12 05:16:27 +0700

    fix(seo): ensure OG tags render correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
-      : process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
+      : "https://our-little-world-blond.vercel.app",
   ),
   openGraph: {
     type: "website",
